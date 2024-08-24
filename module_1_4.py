3f76de8ca4bc86dd57ab3e329aed1087f1f49f93
Андрей <an-shafra2013@ya.ru>
2024-08-24 10:25:44 +0800

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+#Организуйте программу:
+my_string = input("Ваше любимое хобби: ")
+print(18)
+#Работа с методами строк:
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
+
